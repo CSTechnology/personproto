c59532521d73318c49e9472d3fb088d2ff81f029
ubuntuubuntu <ubuntuubuntu@ubuntu>
2012-12-06 17:40:45 +0900

    Signed-off-by: ubuntuubuntu <ubuntuubuntu@ubuntu>

--- project/Build.scala
+++ project/Build.scala
@@ -2,17 +2,21 @@ import sbt._
 import Keys._
 import PlayProject._
 
-object ApplicationBuild extends Build {
-
+object personprotoBuild extends Build {
+	val name			= "personproto"
     val appName         = "personproto"
     val appVersion      = "1.0-SNAPSHOT"
 
-    val appDependencies = Seq(
-      // Add your project dependencies here,
-    )
+	val appDependencies = Seq(
+		  "com.mongodb.casbah" %% "casbah" % "2.1.5-1",
+	      "com.novus" %% "salat-core" % "0.0.8-SNAPSHOT",
+	      "org.scalatest" %% "scalatest" % "2.0.M5",
+	      "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
+     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+          resolvers ++= Seq("repo.novus snaps" at "http://repo.novus.com/snapshots/",
+          "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+          "Sonatype OSS groups" at "https://oss.sonatype.org/content/groups/scala-tools/")
     )
-
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,6 +3,13 @@ logLevel := Level.Warn
 
 // The Typesafe repository 
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+//resolvers += "repository" at "https://oss.sonatype.org/content/groups/scala-tools/"
+//resolvers += "snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+//resolvers += "releases"  at "https://oss.sonatype.org/content/groups/scala-tools"
+//resolvers += "oss.sonatype repository"  at "https://oss.sonatype.org/content/repositories/releases/"
+//resolvers += "scala-tools for Casbah build on heroku release" at "http://scala-tools.org/repo-releases/"
+//resolvers += "scala-tools for Casbah build on heroku snapshot" at "http://scala-tools.org/repo-snapshots/"
+//resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
 
 // Use the Play sbt plugin for Play projects
 addSbtPlugin("play" % "sbt-plugin" % "2.0.4")
