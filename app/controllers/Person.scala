a84384c8e72f6a0759c2f7a6347ec1c976ce6331
ubuntuubuntu <ubuntuubuntu@ubuntu>
2012-12-07 13:15:30 +0900

    Signed-off-by: ubuntuubuntu <ubuntuubuntu@ubuntu>

--- app/controllers/Person.scala
+++ app/controllers/Person.scala
@@ -4,29 +4,33 @@ import play.api._
 import play.api.mvc._
 import play.api.data.Form
 import play.api.data.Forms._
+import models.Person
 
-object Person  extends Controller  {
-  case class User(name:String,age:Int,sex:String)
-  
-    val form = Form(mapping(
-    "name" -> text.verifying("名前は必須入力です。", { name => !name.isEmpty()  }),
+object PersonController extends Controller {
+  val form = Form(mapping(
+    "name" -> text.verifying("名前は必須入力です。", { name => !name.isEmpty() }),
     "age" -> number.verifying("お酒は20歳から99歳まで", { age => age >= 20 && age < 100 }),
-    "sex" -> text)(User.apply)(User.unapply))
-	    
-	  def index = Action { implicit request =>
-      	Ok(views.html.person(form))
-	  }
+    "sex" -> text)(Person.apply)(Person.unapply))
 
-	  def submitEntry = Action { implicit request =>
-	  form.bindFromRequest.fold(
-			errors => BadRequest(views.html.person(errors)),
-	        User => Ok(views.html.person_confirm(User)))
-	    }
-	  
-	  def add = TODO 
-	  
-	  def confirm  = Action{
-	    Ok("hoge")
-	  }
+  def index = Action { implicit request =>
+    Ok(views.html.person(form))
+  }
 
+  def submitEntry = Action { implicit request =>
+    form.bindFromRequest.fold(
+      errors => BadRequest(views.html.person(errors)),
+      person =>
+        {
+          Person.save(person)
+          Ok(views.html.person_confirm(person))
+        })
+  }
+
+  def add = Action { implicit request =>
+    Ok("hoge")
+  }
+
+  def confirm = Action {
+    Ok("hoge")
+  }
 }
--- app/models/Person.scala
+++ app/models/Person.scala
@@ -1,4 +1,26 @@
-import play.api._
-import play.api.mvc._
+package models
+
+import com.novus.salat._
+import com.novus.salat.global._
+import com.mongodb.casbah.Imports._
 import play.api.data.Form
-import play.api.data.Forms._
+
+
+case class Person(name:String,age:Int,sex:String) 
+
+object Person{
+  val collection = MongoConnection()("test")("person")
+  
+  def find = {}
+  
+  def save(person:Person) = {
+    val me = Person(name=person.name, age=person.age, sex=person.sex)
+    val g = grater[Person]
+    collection += g.asDBObject(me)
+    //collection += MongoDBObject("id"->1, "name"->"me", "age"->27)
+  }
+  
+  def delete = {}
+  
+  def update = {}
+}
