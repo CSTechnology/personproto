cd64fa6758745e641bf4a3a663bd88921f850fba
ubuntuubuntu <ubuntuubuntu@ubuntu>
2012-12-07 18:13:45 +0900

    Signed-off-by: ubuntuubuntu <ubuntuubuntu@ubuntu>

--- app/controllers/Person.scala
+++ app/controllers/Person.scala
@@ -5,6 +5,7 @@ import play.api.mvc._
 import play.api.data.Form
 import play.api.data.Forms._
 import models.Person
+import models.PersonDAO
 
 object PersonController extends Controller {
   val form = Form(mapping(
@@ -12,25 +13,37 @@ object PersonController extends Controller {
     "age" -> number.verifying("お酒は20歳から99歳まで", { age => age >= 20 && age < 100 }),
     "sex" -> text)(Person.apply)(Person.unapply))
 
+  //登録画面初期表示
   def index = Action { implicit request =>
     Ok(views.html.person(form))
   }
 
+  //登録
   def submitEntry = Action { implicit request =>
     form.bindFromRequest.fold(
       errors => BadRequest(views.html.person(errors)),
       person =>
         {
-          Person.save(person)
+          PersonDAO.create(person)
           Ok(views.html.person_confirm(person))
         })
   }
 
-  def add = Action { implicit request =>
-    Ok("hoge")
+  //一覧表示
+  def list = Action {
+    val persons = PersonDAO.all()
+    println(persons)
+    Ok(views.html.person_list(persons))
   }
 
-  def confirm = Action {
-    Ok("hoge")
+  def delete = Action {implicit request =>
+    val hoge = form.bindFromRequest.get
+    println(hoge)
+    Ok("削除")
   }
+  //編集
+  def edit(name: String) = Action {
+    Ok("編集")
+  }
+
 }
--- app/models/Person.scala
+++ app/models/Person.scala
@@ -4,23 +4,23 @@ import com.novus.salat._
 import com.novus.salat.global._
 import com.mongodb.casbah.Imports._
 import play.api.data.Form
+import com.novus.salat.annotations._
+import com.novus.salat.dao._
 
+case class Person(name: String, age: Int, sex: String)
 
-case class Person(name:String,age:Int,sex:String) 
+object PersonDAO extends SalatDAO[models.Person, Int](collection = MongoConnection()("test")("person")) {
 
-object Person{
-  val collection = MongoConnection()("test")("person")
-  
-  def find = {}
-  
-  def save(person:Person) = {
-    val me = Person(name=person.name, age=person.age, sex=person.sex)
-    val g = grater[Person]
-    collection += g.asDBObject(me)
-    //collection += MongoDBObject("id"->1, "name"->"me", "age"->27)
+  def all(): List[Person] = PersonDAO.find(MongoDBObject.empty).toList
+
+  def create(person: Person) {
+    PersonDAO.insert(Person(name = person.name, age = person.age, person.sex))
   }
-  
-  def delete = {}
-  
-  def update = {}
+
+  def delete(name: String) {
+    PersonDAO.remove(MongoDBObject("name" -> name))
+  }
+
+  def update = {
+  }
 }
